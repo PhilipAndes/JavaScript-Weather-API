3ea93f2a1d3b4594d1657493818c079dd9af7ab2
Philip Andes <philip__andes@hotmail.nl>
2019-05-09 18:58:56 +0200

    Fetching right info from API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Initialize weather object
-
-const weather = new Weather('Berlin', 'Germany');
+const weather = new Weather('Breda', 'Netherlands');
+// Initialize UI
+const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -10,7 +11,9 @@ document.addEventListener('DOMContentLoaded', getWeather);
 function getWeather() {
     weather.getWeather()
         .then(results => {
-            console.log(results);
+            //console.log(results);
+            //Now we want to take the information and use the method paint, so the idea is, it is painting the ui with the results we get
+            ui.paint(results);
         })
         .catch(err => console.log(err));
 }
